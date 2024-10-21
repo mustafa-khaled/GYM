cac1fc635c23e24e97f53608cfdce2475ebcc539
Mustafa <mostafakhaled0787314@gmail.com>
2024-10-21 21:22:21 +0300

    create diet chart

--- src/features/diet/MealsChart.jsx
+++ src/features/diet/MealsChart.jsx
@@ -1,7 +1,32 @@
+import ProgressCircle from "../../ui/ProgressCircle";
+
 function MealsChart() {
   return (
     <div className="w-full rounded-md bg-bg_color p-[20px] md:w-[50%]">
-      MealsChart
+      <div className="mb-[10px] flex h-[60vh] flex-col items-center justify-center gap-[20px]">
+        <h2 className="text-[20px] font-[700] leading-[24px] md:text-[24px]">
+          السعرات اليوميه
+        </h2>
+
+        <div className="relative flex h-[200px] w-[200px] items-center justify-center rounded-full p-[15px]">
+          <div
+            className="absolute inset-0 rounded-full"
+            style={{
+              background:
+                "conic-gradient(#85FFC5 0 25%, transparent 30% 35%, #FFE8C6 35% 50%, transparent 60% 70%, #FFB445 80% 95%, transparent 95% 100%)",
+            }}
+          />
+          <div className="relative flex h-full w-full items-center justify-center rounded-full bg-bg_color">
+            1500
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-around gap-[20px]">
+        <ProgressCircle title="البروتين" count={11} color={"#85FFC5"} />
+        <ProgressCircle title="الكارب" count={11} color={"#FFE8C6"} />
+        <ProgressCircle title="الجهوت" count={11} color={"#FFB445"} />
+      </div>
     </div>
   );
 }
---
+++ src/ui/ProgressCircle.jsx
@@ -0,0 +1,38 @@
+function ProgressCircle({ title, color, count }) {
+  return (
+    <div className="relative size-[90px]">
+      <svg
+        className="size-full -rotate-90"
+        viewBox="0 0 36 36"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="18"
+          cy="18"
+          r="16"
+          fill="none"
+          className="stroke-current text-gray-200 dark:text-neutral-700"
+          strokeWidth="2"
+        ></circle>
+        <circle
+          cx="18"
+          cy="18"
+          r="16"
+          fill="none"
+          strokeWidth="2"
+          strokeDasharray="100"
+          strokeDashoffset="75"
+          strokeLinecap="round"
+          style={{ stroke: color }}
+        ></circle>
+      </svg>
+
+      <p className="absolute left-[50%] top-[50%] flex translate-x-[-50%] translate-y-[-50%] flex-col items-center gap-[10px] text-sm">
+        <span>{title}</span>
+        <span>{count}</span>
+      </p>
+    </div>
+  );
+}
+
+export default ProgressCircle;
