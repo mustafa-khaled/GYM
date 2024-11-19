6f97b0caf71bb078d57185fcaba1349a9c74389b
Mustafa <mostafakhaled0787314@gmail.com>
2024-11-19 21:56:10 +0200

    form height

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -6,7 +6,7 @@ export default function ContactUs() {
   return (
     <MainSection>
       <Container>
-        <div className="h-fit max-h-[70vh] overflow-y-scroll rounded-lg bg-[#000] p-[20px]">
+        <div className="h-fit overflow-y-scroll rounded-lg bg-[#000] p-[20px]">
           <div className="mb-[30px] text-center">
             <h1 className="text-[24px] font-[700] leading-[24px]">
               تواصل معنا
